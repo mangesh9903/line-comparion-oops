e7768ff2fba6e72b7e751857bca9ec94b2633a6b
mangesh9903 <mangeshb9903@gmail.com>
2022-03-10 08:11:00 +0530

    UC3 Compared Lines Using compareTo Function commited

--- src/com/bridgelabz/service/LineComparator.java
+++ src/com/bridgelabz/service/LineComparator.java
@@ -4,4 +4,6 @@ import com.bridgelabz.model.Line;
 
 public interface LineComparator {
     void compareUsingEquals(Line firstLine, Line secondLine);
+    void compareUsingCompareTo(Line firstLine , Line secondLine);
+
 }
